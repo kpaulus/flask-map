07340261050fd4d7f2dbef1e1984a189d6f3b633
Ken Paulus <kpaulus@appareo.com>
2016-03-09 14:31:15 -0600

    location api

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, jsonify
 from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,21 +14,35 @@ db = client.mapdb
 def map():
 
     _points = db.mapdb.find()
-    points = [point for point in _points]
+    for point in _points:
+        points.append('lat': point.lat, 'lon': point.lon)
 
     return render_template('map.html', points=points)
 
+@app.route('/locations', methods=['GET'])
+def get_locations():
 
-@app.route('/new', methods=['POST'])
-def new():
+    _points = db.mapdb.find()
+    for point in _points:
+        points.append('lat': point.lat, 'lon': point.lon)
+
+    return jsonify({ 'points': points })
+    
+@app.route('/location', methods=['POST'])
+def location():
 
     point_doc = {
-        'lat': request.form['lat'],
-        'lon': request.form['lon']
+        'user': request.json['user'],
+        'lat': request.json['lat'],
+        'lon': request.json['lon'],
+        'timestamp': str(datetime.utcnow())
     }
+
+    # testing on the map
     db.mapdb.insert_one(point_doc)
 
-    return redirect(url_for('map'))
+    #return redirect(url_for('map'))
+    return str(point_doc), 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
